84610620d185ee317273411b014060a8f8676fdf
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-01-07 16:01:36 -0600

    changing naming in subsetting

--- code/R/plot_ancom_asv.R
+++ code/R/plot_ancom_asv.R
@@ -2,8 +2,8 @@ rm(list = ls())
 source("./code/R/00_setup.R")
 source("./code/R/01_load_data.R")
 source("./code/R/02_process_ps.R")
-source("./code/R/03_subset.R")
-source("./code/R/04_subset_DA.R")
+source("./code/R/03_subset_abundance.R")
+source("./code/R/04_subset_significance.R")
 library(writexl)
 library(ComplexHeatmap)
 
@@ -27,11 +27,46 @@ col_fontsize <- 11
 # Fix print genera to Excel  !!!!
 
 # names of significant taxa
-all_sig_taxa <- get_ancom_taxa(ancom_fname, p_threshold, high_ab_genera, write2excel, fname_excel)
+all_sig_taxa <- get_ancom_taxa(ancom_fname, p_threshold, high_ab_OTUs, write2excel, fname_excel)
+
+# !!! Rename OTUs
+sig_taxa_updated <- get_taxonomy(ps) %>%
+  filter(OTU %in% all_sig_taxa) %>%
+  mutate(
+    Species_tmp = case_when(
+      !is.na(Species) & !startsWith(Species, "midas") ~ Species,
+      !is.na(Genus)   & !startsWith(Genus, "midas")   ~ paste0(Genus, "_sp_g"),
+      !is.na(Family)  & !startsWith(Family, "midas")  ~ paste0(Family, "_sp_f"),
+      !is.na(Order)   & !startsWith(Order, "midas")   ~ paste0(Order, "_sp_o"),
+      !is.na(Class)   & !startsWith(Class, "midas")   ~ paste0(Class, "_sp_c")
+    )
+  ) %>%
+  group_by(Species_tmp) %>%
+  mutate(
+    Species_updated = if (n() == 1) {
+      Species_tmp
+    } else {
+      paste0(Species_tmp, "-", row_number())  
+    }
+  ) %>%
+  ungroup() %>%
+  select(OTU, Species_updated)
 
 # ----- Subset ------
-high_DA_taxa <- high_ab_OTUs[high_ab_OTUs %in% all_sig_taxa]
-low_DA_taxa <- all_sig_taxa[!all_sig_taxa %in% high_DA_taxa]
+high_ab_taxa <- intersect(all_sig_taxa, high_ab_OTUs)
+low_ab_taxa <- setdiff(all_sig_taxa, high_ab_OTUs)
+
+high_ab_taxa_updated <- sig_taxa_updated$Species_updated[
+  match(high_ab_taxa, sig_taxa_updated$OTU)
+]
+
+low_ab_taxa_updated <- sig_taxa_updated$Species_updated[
+  match(low_ab_taxa, sig_taxa_updated$OTU)
+]
+
+
+
+
 
 # Define matrix of log-fold change data per size
 process_lfc <- function(df, taxa) {
@@ -43,46 +78,13 @@ process_lfc <- function(df, taxa) {
       XL  = ifelse(q_size.nameXL  < p_threshold & passed_ss_size.nameXL,  lfc_size.nameXL, 0),
       XXL = ifelse(q_size.nameXXL < p_threshold & passed_ss_size.nameXXL, lfc_size.nameXXL, 0)
     ) %>%
-    dplyr::select(Species, M, L, XL, XXL) %>% # Genus - name_numbered
+    dplyr::select(name_numbered, M, L, XL, XXL) %>% # Species
     mutate(
       mean_lfc = rowMeans(across(M:XXL, abs), na.rm = TRUE)
     ) %>%
     rename_with(~ paste0(., "-S"), c("M", "L", "XL", "XXL")) %>%
     arrange(desc(mean_lfc))
 }
-
-# !!! Rename OTUs
-taxonomy <- get_taxonomy(ps)
-
-output <- readRDS(ancom_fname)
-
-res_prim = output$res %>% 
-  rename(OTU = taxon) %>%
-  left_join(taxonomy, by = "OTU") %>%
-  mutate(
-    Species_updated = case_when(
-      !is.na(Species) & !startsWith(Species, "midas") ~ Species,
-      !is.na(Genus)   & !startsWith(Genus, "midas")   ~ paste0(Genus, "_sp_g"),
-      !is.na(Family)  & !startsWith(Family, "midas")  ~ paste0(Family, "_sp_f"),
-      !is.na(Order)   & !startsWith(Order, "midas")   ~ paste0(Order, "_sp_o"),
-      !is.na(Class)   & !startsWith(Class, "midas")   ~ paste0(Class, "_sp_c")
-    )
-  ) %>%
-  group_by(Species_updated) %>%
-  mutate(
-    name_numbered = if (n() == 1) {
-      Species_updated
-    } else {
-      paste0(Species_updated, "-", row_number())
-    }
-  ) %>%
-  ungroup()
-
-
-# all_sig_taxa <- taxonomy %>%
-#   filter(OTU %in% all_sig_ASV) %>%
-#   pull(Genus) %>%
-#   unique() 
 
 
 
@@ -94,13 +96,13 @@ lfc_low  <- process_lfc(res_prim, low_DA_taxa)
 ### For plotting
 fig_high <- lfc_high %>%
   dplyr::select(-mean_lfc) %>%
-  tibble::column_to_rownames("Species") %>%
+  tibble::column_to_rownames("name_numbered") %>%
   as.matrix()
 
 fig_low <- lfc_low %>%
   head(n_display_low) %>%
   dplyr::select(-mean_lfc) %>%
-  tibble::column_to_rownames("Species") %>%
+  tibble::column_to_rownames("name_numbered") %>%
   as.matrix()
 
 # ---- Plotting
