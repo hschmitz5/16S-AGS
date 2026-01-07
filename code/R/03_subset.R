67a9246db201fe3c4d9381a2792d893cf35dbfb7
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-01-07 12:59:38 -0600

    needs to be in OTU for DA

--- code/R/03_subset.R
+++ code/R/03_subset.R
@@ -16,30 +16,9 @@ get_OTUs_above_cutoff <- function(ps){
   # Few have ASVs with a non-negligible abundance
   OTU_subset <- get_rel_ASV(ps) %>%
     filter(Abundance > rel_ab_cutoff) %>%
-    # rename OTUs
-    mutate(
-      Species_tmp = case_when(
-        !is.na(Species) & !startsWith(Species, "midas") ~ Species,
-        !is.na(Genus)   & !startsWith(Genus, "midas")   ~ paste0(Genus, "_sp_g"),
-        !is.na(Family)  & !startsWith(Family, "midas")  ~ paste0(Family, "_sp_f"),
-        !is.na(Order)   & !startsWith(Order, "midas")   ~ paste0(Order, "_sp_o"),
-        !is.na(Class)   & !startsWith(Class, "midas")   ~ paste0(Class, "_sp_c")
-      )
-    ) %>%
-    group_by(Species_tmp) %>%
-    mutate(
-      Species_updated = if (n() == 1) {
-        Species_tmp
-      } else {
-        paste0(Species_tmp, "-", row_number())  
-      }
-    ) %>%
-    ungroup() %>%
-    select(-Species_tmp) %>% # delete column
     arrange(desc(Abundance)) 
 }
-
-
+  
 # Define genera above rel_ab_cutoff
 genus_avg <- get_avg_genus(ps)
 high_ab_genera <- genus_avg$Genus
@@ -47,5 +26,5 @@ rm(genus_avg)
 
 # Define OTUs above rel_ab_cutoff
 OTU_subset <- get_OTUs_above_cutoff(ps)
-high_ab_OTUs <- OTU_subset$Species_updated 
+high_ab_OTUs <- OTU_subset$OTU
 rm(OTU_subset)
