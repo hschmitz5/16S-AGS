9773e35a360e9699e58b3e4d45298b2dc2f4b246
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-01-08 13:37:55 -0600

    updated naming

--- code/R/plot_ancom_asv.R
+++ code/R/plot_ancom_asv.R
@@ -2,12 +2,11 @@ rm(list = ls())
 source("./code/R/00_setup.R")
 source("./code/R/01_load_data.R")
 source("./code/R/02_process_ps.R")
-source("./code/R/03_subset_abundance.R")
-source("./code/R/04_subset_significance.R")
+source("./code/R/03_subset.R")
 library(writexl)
 library(ComplexHeatmap)
 
-write2excel <- TRUE
+write2excel <- FALSE
 n_display_low <- 30
 
 ancom_fname <- "./data/ancombc2_ASV.rds"
@@ -24,61 +23,27 @@ cell_w <- 0.6 # same as cell_h
 row_fontsize <- 10
 col_fontsize <- 11
 
-# Fix print genera to Excel  !!!!
-
 # names of significant taxa
-all_sig_taxa <- get_ancom_taxa(ancom_fname, p_threshold, high_ab_OTUs, write2excel, fname_excel)
+sig_taxa <- get_ancom_taxa(ancom_fname, ps, p_threshold, rel_ab_cutoff, write2excel, fname_excel)
 
-# !!! Rename OTUs
-sig_taxa_updated <- get_taxonomy(ps) %>%
-  filter(OTU %in% all_sig_taxa) %>%
-  mutate(
-    Species_tmp = case_when(
-      !is.na(Species) & !startsWith(Species, "midas") ~ Species,
-      !is.na(Genus)   & !startsWith(Genus, "midas")   ~ paste0(Genus, "_sp_g"),
-      !is.na(Family)  & !startsWith(Family, "midas")  ~ paste0(Family, "_sp_f"),
-      !is.na(Order)   & !startsWith(Order, "midas")   ~ paste0(Order, "_sp_o"),
-      !is.na(Class)   & !startsWith(Class, "midas")   ~ paste0(Class, "_sp_c")
-    )
-  ) %>%
-  group_by(Species_tmp) %>%
-  mutate(
-    Species_updated = if (n() == 1) {
-      Species_tmp
-    } else {
-      paste0(Species_tmp, "-", row_number())  
-    }
-  ) %>%
-  ungroup() %>%
-  select(OTU, Species_updated)
-
-# ----- Subset ------
-high_ab_taxa <- intersect(all_sig_taxa, high_ab_OTUs)
-low_ab_taxa <- setdiff(all_sig_taxa, high_ab_OTUs)
-
-high_ab_taxa_updated <- sig_taxa_updated$Species_updated[
-  match(high_ab_taxa, sig_taxa_updated$OTU)
-]
-
-low_ab_taxa_updated <- sig_taxa_updated$Species_updated[
-  match(low_ab_taxa, sig_taxa_updated$OTU)
-]
-
-
-
-
+# Read in data and add new names
+output <- readRDS(ancom_fname)
+res_prim <- output$res %>% 
+  rename(OTU = taxon) %>%
+  left_join(sig_taxa$name_tbl, join_by(OTU)) 
 
 # Define matrix of log-fold change data per size
 process_lfc <- function(df, taxa) {
   df %>%
-    filter(OTU %in% taxa) %>%
+    filter(Species_updated %in% taxa) %>%
+    # change value to zero if not significant
     mutate(
       M   = ifelse(q_size.nameM   < p_threshold & passed_ss_size.nameM,   lfc_size.nameM, 0),
       L   = ifelse(q_size.nameL   < p_threshold & passed_ss_size.nameL,   lfc_size.nameL, 0),
       XL  = ifelse(q_size.nameXL  < p_threshold & passed_ss_size.nameXL,  lfc_size.nameXL, 0),
       XXL = ifelse(q_size.nameXXL < p_threshold & passed_ss_size.nameXXL, lfc_size.nameXXL, 0)
     ) %>%
-    dplyr::select(name_numbered, M, L, XL, XXL) %>% # Species
+    dplyr::select(Species_updated, M, L, XL, XXL) %>% # Species - new name
     mutate(
       mean_lfc = rowMeans(across(M:XXL, abs), na.rm = TRUE)
     ) %>%
@@ -86,23 +51,20 @@ process_lfc <- function(df, taxa) {
     arrange(desc(mean_lfc))
 }
 
-
-
-
 # Apply function to high and low abundance taxa
-lfc_high <- process_lfc(res_prim, high_DA_taxa)
-lfc_low  <- process_lfc(res_prim, low_DA_taxa)
+lfc_high <- process_lfc(res_prim, sig_taxa$high_ab)
+lfc_low  <- process_lfc(res_prim, sig_taxa$low_ab)
 
 ### For plotting
 fig_high <- lfc_high %>%
   dplyr::select(-mean_lfc) %>%
-  tibble::column_to_rownames("name_numbered") %>%
+  tibble::column_to_rownames("Species_updated") %>%
   as.matrix()
 
 fig_low <- lfc_low %>%
   head(n_display_low) %>%
   dplyr::select(-mean_lfc) %>%
-  tibble::column_to_rownames("name_numbered") %>%
+  tibble::column_to_rownames("Species_updated") %>%
   as.matrix()
 
 # ---- Plotting
