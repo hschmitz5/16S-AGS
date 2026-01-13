d21fdd1c93a7921f7be562f13ecbc9467a359dee
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-01-13 15:13:14 -0600

    refined legends

--- code/R/plot_rel_heatmap.R
+++ code/R/plot_rel_heatmap.R
@@ -2,7 +2,6 @@ source("./code/R/00_setup.R")
 source("./code/R/01_load_data.R")
 source("./code/R/02_process_ps.R")
 source("./code/R/03_subset.R")
-source("./code/R/04_subset_DA.R")
 library(ComplexHeatmap)
 
 fname_rel <- "./figures/rel_ab_heatmap.png"
@@ -16,77 +15,86 @@ row_fontsize <- 10
 col_fontsize <- 11
 
 # Choose DA Taxa
-all_taxa_ancom <- get_ancom_taxa(ancom_fname, p_threshold, write2excel = FALSE)
-DA_taxa <- high_ab_genera[high_ab_genera %in% all_taxa_ancom]
+ancom_taxa <- get_ancom_taxa(ancom_fname, ps, p_threshold, rel_ab_cutoff, write2excel = FALSE)
+DA_taxa <- ancom_taxa$high_ab 
+
 # Common taxa (ANCOM & ALDEx)
-# DA_taxa <- get_common_taxa(ancom_fname, aldex_fname, p_threshold, effect_threshold, high_ab_genera)
+#DA_taxa2 <- get_common_taxa(ancom_fname, aldex_fname, p_threshold, effect_threshold)
 
 # -------- Define groups ---------
 
-metadata <- data.frame(ps@sam_data) %>% .[sam_name, ] # reorder
+high_ab_taxa <- get_rel_ASV(ps) %>%
+  filter(Abundance > rel_ab_cutoff) %>%
+  distinct(OTU) %>%
+  pull(OTU)
 
-# Define colors for size categories directly from metadata
-size_colors <- setNames(met.brewer(size_pal, n_sizes), size$name)
-
-# Create the annotation directly from metadata
-bot_annot <- HeatmapAnnotation(
-  Size = factor(metadata$size.name, levels = size$name),
-  col = list(Size = size_colors)
-)
-
-# ---- Plotting
-
-make_rel_heatmap <- function(df, annot = NULL, legend_name = "Rel Ab [%]") {
-  data_mat <- df %>%
-    column_to_rownames("Genus") %>%
-    as.matrix() %>%
-    .[, sam_name]  # reorder columns 
-  
-  n_cols <- ncol(data_mat)
-  n_rows <- nrow(data_mat)
-  row_fontface <- ifelse(rownames(data_mat) %in% DA_taxa, "bold", "plain")
-  
-  Heatmap(
-    data_mat,
-    name = legend_name,
-    cluster_columns = FALSE,
-    show_row_names = TRUE,
-    show_column_names = FALSE,
-    column_names_rot = 0,
-    column_names_centered = TRUE,
-    bottom_annotation = annot,
-    col = met.brewer("Hokusai2", type = "continuous"),
-    width  = unit(n_cols * cell_w, "inches"),
-    height = unit(n_rows * cell_h, "inches"),
-    row_names_gp = gpar(fontsize = row_fontsize, fontface = row_fontface),
-    column_names_gp = gpar(fontsize = col_fontsize)
-  )
-}
-
-rel_wide <- get_rel_genus(ps) %>%
-  filter(Genus %in% high_ab_genera) %>%
-  dplyr::select(Genus, Sample, Abundance) %>%  
+rel_wide <- get_rel_ASV(ps) %>%
+  filter(OTU %in% high_ab_taxa) %>%
+  dplyr::select(OTU, Sample, Abundance) %>%  
   pivot_wider(
     names_from = Sample,
     values_from = Abundance
   ) 
 
-rel_wide_top  <- rel_wide %>%
-  filter(Genus %in% high_ab_genera[1:3])
+# pseudo <- 1e-6  # choose based on detection limit
+data_mat <- rel_wide %>%
+  column_to_rownames("OTU") %>%
+  as.matrix() %>%
+  .[, sam_name] 
+# %>% # reorder columns 
+#   { log10(. + pseudo) }
 
-rel_wide_rest <- rel_wide %>%
-  filter(Genus %in% high_ab_genera[4:length(high_ab_genera)])
 
-ht_top  <- make_rel_heatmap(rel_wide_top, NULL, legend_name = "Top 3\nRel Ab [%]")
-ht_rest <- make_rel_heatmap(rel_wide_rest, bot_annot, legend_name = "Bottom\nRel Ab [%]")
-ht_list <- ht_top %v% ht_rest
+# ---- Plotting
+n_cols <- ncol(data_mat)
+n_rows <- nrow(data_mat)
+split = rep(1:n_sizes, each = n_replicates)
+
+row_fontface <- ifelse(rownames(data_mat) %in% DA_taxa, "bold", "plain")
+
+ht_colors <- met.brewer("Hokusai2", type = "continuous")
+ha_colors <- met.brewer(size_pal, n_sizes)
+
+size_annot <- HeatmapAnnotation(sz = anno_block(gp = gpar(fill = ha_colors),
+                                                labels = size$name,
+                                                labels_gp = gpar(col = "white", fontsize = col_fontsize)))
+
+ht <- Heatmap(
+  data_mat,
+  column_split = split,
+  top_annotation = size_annot,
+  name = "Rel Ab [%]",
+  cluster_columns = FALSE,
+  show_heatmap_legend = FALSE,
+  show_row_names = TRUE,
+  show_column_names = FALSE,
+  column_names_rot = 0,
+  column_names_centered = TRUE,
+  column_title = NULL,
+  col = ht_colors,
+  width  = unit(n_cols * cell_w, "inches"),
+  height = unit(n_rows * cell_h, "inches"),
+  row_names_gp = gpar(fontsize = row_fontsize, fontface = row_fontface),
+  column_names_gp = gpar(fontsize = col_fontsize)
+)
+
+lgd <- Legend(
+  col_fun = colorRamp2(
+    seq(min(data_mat), max(data_mat), length.out = length(ht_colors)),
+    ht_colors
+  ),
+  title = "Rel Ab [%]",
+  direction = "horizontal",
+  title_position = "lefttop",
+  at = pretty(c(min(data_mat), max(data_mat))),
+  labels = pretty(c(min(data_mat), max(data_mat)))
+)
 
 # Draw combined heatmap
 png(fname_rel,
-    width = 6,  # width in inches; can adjust
-    height = 8, # height in inches; can adjust
+    width = 8,  # width in inches; can adjust
+    height = 12, # height in inches; can adjust
     units = "in", res = 300)
-draw(ht_list,
-     heatmap_legend_side = "right",
-     annotation_legend_side = "top")
+draw(ht)
+draw(lgd, x = unit(0.41, "npc"), y = unit(0.96, "npc"), just = c("center", "top"))
 dev.off()
