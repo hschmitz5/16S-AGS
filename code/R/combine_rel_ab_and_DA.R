b964fdcb96e53a82ba0b1524e540766783ea4490
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-01-21 16:11:43 -0600

    makes subplot of rel ab and DA plots

---
+++ code/R/combine_rel_ab_and_DA.R
@@ -0,0 +1,32 @@
+library(magick)
+
+fname_out <- "./figures/rel_ab_DA_combined.png"
+
+# Read images
+img_left  <- image_read(fname_rel)
+img_right <- image_read(fname_high)
+
+# Get image heights
+height_left  <- image_info(img_left)$height
+height_right <- image_info(img_right)$height
+
+# Get image heights
+height_left  <- image_info(img_left)$height
+height_right <- image_info(img_right)$height
+
+# If the right image is shorter, add vertical padding to center it
+if (height_right < height_left) {
+  # New geometry string: WIDTHxHEIGHT
+  new_geometry <- paste0(image_info(img_right)$width, "x", height_left)
+  
+  # Add transparent padding, center the original image
+  img_right <- image_extent(img_right,
+                            geometry = new_geometry,
+                            gravity = "center") 
+}
+
+# Combine horizontally
+combined <- image_append(c(img_left, img_right))
+
+# Save the combined image
+image_write(combined, path = fname_out)
