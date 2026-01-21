a09f2bccf9b9d5ea31e8ef3a72bca557f0591d68
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-01-21 14:11:35 -0600

    changed genus to OTU

--- code/R/04_bias_correct.R
+++ code/R/04_bias_correct.R
@@ -5,11 +5,11 @@ get_bc_abund <- function(fname) {
   metadata <- get_metadata(ps)
   
   bc_long <- output$bias_correct_log_table %>%
-    rownames_to_column("Genus") %>%
-    filter(!is.na(Genus)) %>%
-    pivot_longer(-Genus, names_to = "Sample", values_to = "bc_abund") %>%
+    rownames_to_column("OTU") %>%
+    filter(!is.na(OTU)) %>%
+    pivot_longer(-OTU, names_to = "Sample", values_to = "bc_abund") %>%
     left_join(metadata, by = "Sample") %>%
-    dplyr::select(Sample, size.mm, size.name, Genus, bc_abund) 
+    dplyr::select(Sample, size.mm, size.name, OTU, bc_abund) 
   
   return(bc_long)
 }
