ecd34920b2d9ca0deadfdc9cdc3c094c0d3bd403
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-01-21 14:12:16 -0600

    added on mu

--- code/R/plot_ordination.R
+++ code/R/plot_ordination.R
@@ -3,6 +3,7 @@ source("./code/R/00_setup.R")
 source("./code/R/01_load_data.R")
 
 fname_ord <- "./figures/ordination-PCoA.png"
+fname_ord2 <- "./figures/ordination-PCoA-mu.png"
 
 # load phyloseq object for all sample sizes
 ps_full <- readRDS("./data/ps_ASV_full.rds")
@@ -23,3 +24,22 @@ p <- plot_ordination(ps_full, ps.ord, type="samples", color="size.name") +
 ordination_plot <- p
 
 ggsave(fname_ord, plot = ordination_plot, width = 5, height = 3, dpi = 300)
+
+# Add mu to ps
+ps@sam_data$mu <- mu$mu[ match(ps@sam_data$size.name, mu$size.name) ]
+
+ps2.ord <- ordinate(ps, "PCoA", "wunifrac")
+
+p2 <- plot_ordination(ps, ps2.ord, type="samples", color="mu") +
+  labs(title="PCoA (wunifrac)", color = "Mu") +
+  theme_minimal(base_size = 12) +
+  theme(
+    plot.title = element_text(size = 12)
+  ) 
+
+ordination_plot_mu <- p2
+
+ggsave(fname_ord2, plot = ordination_plot_mu, width = 5, height = 3, dpi = 300)
+
+# library(vegan)
+# ord <- metaMDS(ps, trace = FALSE)
