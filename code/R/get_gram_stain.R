4d8f48bed2244e5d2663d0b8e7a71bfa21d3336b
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-02-05 12:53:05 -0600

    new code

---
+++ code/R/get_gram_stain.R
@@ -0,0 +1,86 @@
+# This code only works for genus or species
+
+rm(list = ls())
+source("./code/R/00_setup.R")
+source("./code/R/01_load_data.R")
+source("./code/R/02_process_ps.R")
+source("./code/R/03_subset.R")
+library(BacDive)
+library(purrr)
+
+fname_out_partial <- "./data/bd_gram_stain_partial.rds"
+fname_out_final   <- "./data/bd_gram_stain.rds"
+
+# Choose DA Taxa
+ancom_taxa <- get_ancom_taxa(ancom_fname, ps, p_threshold, rel_ab_cutoff, write2excel = FALSE)
+DA_taxa <- ancom_taxa$high_ab 
+
+DA_genera <- taxonomy %>%
+  filter(Species_updated %in% DA_taxa) %>%
+  mutate(
+    g_suffix = if_else(
+      str_detect(Species_updated, "_g-[0-9]+$"),
+      str_replace(Species_updated, "_g-[0-9]+$", ""),
+      NA_character_
+    )
+  ) %>%
+  pull(g_suffix) %>%
+  na.omit() %>%
+  unique()
+
+#####
+
+bd <- open_bacdive("hannah.schmitz@northwestern.edu", "QP4,@,_nunfQEY6")
+
+safe_retrieve <- function(g, max_tries = 3, wait = 2) {
+  for (i in seq_len(max_tries)) {
+    res <- tryCatch(
+      retrieve(object = bd, query = g, search = "taxon"),
+      error = function(e) NULL
+    )
+    
+    if (!is.null(res)) return(res)
+    
+    message("  ⏳ Retry ", i, " for ", g)
+    Sys.sleep(wait)
+  }
+  NULL
+}
+
+all_gram_df <- tibble()
+
+for (g in DA_genera) {
+  
+  message("Processing: ", g)
+  
+  records <- safe_retrieve(g)
+  
+  if (is.null(records)) {
+    message("  ❌ Giving up on ", g)
+    next
+  }
+  
+  gram_df <- map_dfr(records, ~ {
+    tibble(
+      ID = as.character(pluck(.x, "General", "BacDive-ID", .default = NA)),
+      gram_stain = pluck(
+        .x,
+        "Morphology", "cell morphology", "gram stain",
+        .default = NA_character_
+      ),
+      query = g
+    )
+  })
+  
+  all_gram_df <- bind_rows(all_gram_df, gram_df)
+  Sys.sleep(1)
+  
+  saveRDS(all_gram_df, fname_out_partial)
+}
+
+saveRDS(all_gram_df, fname_out_final)
+
+gram_counts <- all_gram_df %>%
+  filter(!is.na(gram_stain)) %>%
+  group_by(query, gram_stain) %>%
+  summarise(n = n(), .groups = "drop")
