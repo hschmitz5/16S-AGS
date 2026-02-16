5b86feebe47baf0556ada2b083fa4acb42d2dcf6
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-02-16 14:36:39 -0600

    commented out code I used to calculate mean Ca. Contendobacter

--- code/R/plot_rel_scatter.R
+++ code/R/plot_rel_scatter.R
@@ -23,9 +23,17 @@ ASV_size <- get_rel_ASV(ps) %>%
     .groups = "drop"
   ) %>%
   filter(OTU %in% sig_taxa$high_ab[1:n_display]) %>%
-  mutate(OTU = factor(OTU, levels = sig_taxa$high_ab[1:n_display]))
+  mutate(OTU = factor(OTU, levels = sig_taxa$high_ab[1:n_display])) 
 
-# italicize some lobels
+# %>%
+#   filter(OTU %in% "Ca_Contendobacter_g-18") %>%
+#   group_by(size.name) %>%
+#   summarise(
+#     Avg_Abund = mean(Abundance),
+#     .groups = "drop"
+#   ) 
+
+# italicize some labels
 otu_levels <- levels(ASV_size$OTU)
 
 italic_rows <- 
@@ -64,7 +72,7 @@ p <- ggplot(ASV_size, aes(x = Sample, y = Abundance, colour = OTU, shape = OTU))
     strip.placement = "outside",      # place strips below the panel
     strip.text.x = element_text(size = 10, margin = margin(t = 5)),
     legend.text = element_markdown()  # italicize labels
-  )
+  ) 
 
 # Save plot
 ggsave(fname, plot = p, width = 6, height = 4, dpi = 300)
