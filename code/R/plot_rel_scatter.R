9070b9dd35b77fff7ac2b36197d9a142c7115fe7
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-02-26 13:29:43 -0600

    added taxa_order to factor the taxa in descending order

--- code/R/plot_rel_scatter.R
+++ code/R/plot_rel_scatter.R
@@ -1,29 +1,50 @@
+rm(list = ls())
 library(ggtext)
 source("./code/R/00_setup.R")
 source("./code/R/01_load_data.R")
 source("./code/R/02_process_ps.R")
 source("./code/R/03_subset.R")
 
-# display top n most abundant genera
-n_display <- 5
-
-shapes <- c(16, 17, 15, 18, 3)
-
 fname <- "./figures/abund_size_scatter.png"
 
-# names of significant taxa
-write2excel = FALSE
-sig_taxa <- get_ancom_taxa(ancom_fname, ps, p_threshold, rel_ab_cutoff, write2excel, fname_excel)
+shapes <- c(1, 17, 0, 16, 2, 15, 1) 
 
+# names of significant taxa
+# write2excel = FALSE
+# sig_taxa <- get_ancom_taxa(ancom_fname, ps, p_threshold, rel_ab_cutoff, write2excel, fname_excel)
+# plot_taxa <- sig_taxa$high_ab[1:n_display]
+# filter(OTU %in% plot_taxa) 
+
+plot_genus <- "Ca_Competibacter"
+
+# 1) Taxa passing cutoff in any group
+plot_taxa <- get_rel_ASV(ps) %>%
+  filter(startsWith(OTU, plot_genus),
+         Abundance > rel_ab_cutoff) %>%
+  distinct(OTU) %>%
+  pull(OTU)
+
+# 2) Summarise once
 ASV_size <- get_rel_ASV(ps) %>%
-  dplyr::select(Sample, size.mm, size.name, OTU, Abundance) %>%
-  group_by(Sample, size.mm, size.name, OTU) %>%
+  filter(OTU %in% plot_taxa) %>%
+  group_by(size.mm, size.name, OTU) %>%
   summarise(
-    Abundance = sum(Abundance),
+    mean_ab = mean(Abundance),
+    std_ab  = sd(Abundance),
     .groups = "drop"
-  ) %>%
-  filter(OTU %in% sig_taxa$high_ab[1:n_display]) %>%
-  mutate(OTU = factor(OTU, levels = sig_taxa$high_ab[1:n_display])) 
+  )
+
+# 3) Derive ordering from that summary
+taxa_order <- ASV_size %>%
+  filter(size.name == "S") %>%
+  arrange(desc(mean_ab)) %>%
+  pull(OTU)
+
+# 4) Apply factor levels
+ASV_size <- ASV_size %>%
+  mutate(OTU = factor(OTU, levels = taxa_order))
+
+n_display <- length(plot_taxa)
 
 # italicize some labels
 otu_levels <- levels(ASV_size$OTU)
@@ -39,10 +60,13 @@ otu_labels <- ifelse(
 
 names(otu_labels) <- otu_levels
 
-p <- ggplot(ASV_size, aes(x = Sample, y = Abundance, colour = OTU, shape = OTU)) +
-  # group samples by size
-  facet_grid(. ~ size.name, scales = "free_x", space = "free_x", switch = "x") +
-  geom_point() + 
+p <- ggplot(ASV_size, aes(x = size.name, y = mean_ab, colour = OTU, shape = OTU)) +
+  geom_point(position = position_dodge(width = 0.2), size = 2) + 
+  geom_errorbar(
+    aes(ymin = mean_ab - std_ab, ymax = mean_ab + std_ab),
+    width = 0.2,
+    position = position_dodge(width = 0.2)
+  ) +
   scale_colour_manual(
     values = met.brewer(taxa_pal, n_display),
     labels = otu_labels,
@@ -59,12 +83,8 @@ p <- ggplot(ASV_size, aes(x = Sample, y = Abundance, colour = OTU, shape = OTU))
   ) +
   theme_minimal(base_size = 10) +
   theme(
-    axis.text.x = element_blank(),    # hide sample labels
-    axis.ticks.x = element_blank(),
-    strip.placement = "outside",      # place strips below the panel
-    strip.text.x = element_text(size = 10, margin = margin(t = 5)),
-    legend.text = element_markdown()  # italicize labels
-  ) 
+    legend.text = element_markdown()
+  )
 
 # Save plot
 ggsave(fname, plot = p, width = 6, height = 4, dpi = 300)
