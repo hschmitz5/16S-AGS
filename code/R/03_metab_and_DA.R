40efc5d583032b918c18bbeb7e30f0ed47b79625
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-06-02 18:38:14 -0500

    consolidated DA processing

--- code/R/03_metab_and_DA.R
+++ code/R/03_metab_and_DA.R
@@ -1,3 +1,36 @@
+get_metabolism <- function(df, metab_fname) {
+  m <- read_excel(metab_fname, sheet = "input") # tibble
+  
+  metab <- df %>%
+    dplyr::select(Genus, OTU) %>%
+    distinct() %>%
+    left_join(., m, by = "Genus") %>%
+    dplyr::select(-Genus) %>%
+    column_to_rownames("OTU")
+}
+
+get_ancom_taxa <- function(fname_in, ps, p_threshold) {
+  # load differential abundance data
+  output <- readRDS(fname_in)
+  
+  taxonomy <- get_taxonomy(ps)
+  
+  # Subset based on significance and passing the sensitivity analysis
+  all_sig_taxa <- output$res %>%
+    rename(OTU = taxon) %>%
+    left_join(taxonomy, join_by(OTU)) %>%
+    filter(!is.na(Phylum)) %>%
+    # Combines diff_size* and passed_ss* together
+    pivot_longer(
+      cols = matches("q_size\\.name|passed_ss_size\\.name"),
+      names_to = c(".value","size"),
+      names_pattern = "(q|passed_ss)_size\\.name(.*)"
+    ) %>%
+    filter(q < p_threshold & passed_ss == TRUE) %>%   
+    distinct(OTU) %>%
+    pull(OTU)
+}
+
 get_rel_agglom <- function(ps, ancom_fname, rel_ab_cutoff, p_threshold) { 
   # define taxa in which at least one sample has abundance > rel_ab_cutoff
   high_ab_taxa <- get_rel_ASV(ps) %>%
@@ -48,4 +81,22 @@ get_rel_agglom <- function(ps, ancom_fname, rel_ab_cutoff, p_threshold) {
       )
     ) %>%
     select(Genus, OTU, DA, all_of(sam_name))
+}
+
+# Only the difference between bias corrected data is meaningful.
+# It is not a substitute for relative abundance.
+get_bc_abund <- function(fname) {
+  
+  output <- readRDS(fname)
+  
+  metadata <- get_metadata(ps)
+  
+  bc_long <- output$bias_correct_log_table %>%
+    rownames_to_column("OTU") %>%
+    filter(!is.na(OTU)) %>%
+    pivot_longer(-OTU, names_to = "Sample", values_to = "bc_abund") %>%
+    left_join(metadata, by = "Sample") %>%
+    dplyr::select(Sample, size.mm, size.name, OTU, bc_abund) 
+  
+  return(bc_long)
 }
