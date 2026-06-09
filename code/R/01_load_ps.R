73984811777faee8b7cd1e1bb873cd8983d0f295
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-06-09 10:07:23 -0500

    added rel_wide and removed writing over phyloseq label names

--- code/R/01_load_ps.R
+++ code/R/01_load_ps.R
@@ -10,19 +10,16 @@ suppressPackageStartupMessages({
 })
 knitr::opts_chunk$set(echo = TRUE, message = FALSE, warning = FALSE, cache = TRUE)
 
+
+# load phyloseq object (absolute counts)
+ps_fname <- "./data/ps_genus_subset.rds"
+ps <- readRDS(ps_fname)
+
+
 # Color Palettes (MetBrewer)
 size_pal <- "Greek"     # reds
 taxa_pal <- "Hiroshige" # orange, blue
 
-# Relative Abundance Cutoff (%) 
-rel_ab_cutoff <- 0.5
-
-# p-value used for defining significant taxa in differential abundance
-p_threshold   <- 0.05
-
-# load phyloseq object (absolute counts)
-ps_fname <- "./data/ps_ASV_subset.rds"
-ps <- readRDS(ps_fname)
 
 # ------ Define size groups ----------
 
@@ -40,30 +37,34 @@ size <- data.frame(
 sam_name <- c("20A", "20B", "20C", "14A", "14B", "14C", "10A", "10B", "10C",
               "7A", "7B", "7C", "5A", "5B", "5C")
 
-# ------ Change OTU to Species_updated --------
-
-taxonomy <- data.frame(tax_table(ps))
-rownames(ps@otu_table) <- taxonomy$Species_updated
-rownames(ps@tax_table) <- taxonomy$Species_updated
-ps@phy_tree$tip.label <- taxonomy$Species_updated
-rm(taxonomy)
 
 # ------ Functions for processing phyloseq object ----
 
 get_metadata <- function(ps){
-  metadata <- data.frame(sample_data(ps)) %>%
+  metadata <- as.data.frame(sample_data(ps)) %>%
     tibble::rownames_to_column("Sample") %>%
     arrange(size.name)
 }
 
 get_taxonomy <- function(ps){
-  taxonomy <- data.frame(tax_table(ps)) %>%
+  taxonomy <- as.data.frame(tax_table(ps)) %>%
     tibble::rownames_to_column("OTU")
 }
 
-get_rel_ASV <- function(ps) {
+get_rel <- function(ps) {
   # define relative abundance
-  ps_rel_ASV <- phyloseq::transform_sample_counts(ps, function(x) x*100/sum(x))
-  
-  phyloseq::psmelt(ps_rel_ASV)
+  ps_rel <- phyloseq::transform_sample_counts(ps, function(x) x*100/sum(x))
+  # combines taxonomy and abundance
+  phyloseq::psmelt(ps_rel)
+}
+
+get_rel_wide <- function(ps) {
+  rel_wide <- get_rel(ps) %>%
+    dplyr::select(Genus, Sample, Abundance) %>%  
+    pivot_wider(
+      names_from = Sample,
+      values_from = Abundance
+    ) %>%
+    column_to_rownames(var = "Genus") %>%
+    dplyr::select(all_of(sam_name)) 
 }
