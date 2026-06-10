0223cddee9e98488c4f35b5763c7632694d133dc
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-06-10 13:13:51 -0500

    changed legend units to relative abundance

--- code/R/plot_rel_heatmap.R
+++ code/R/plot_rel_heatmap.R
@@ -1,5 +1,6 @@
 rm(list = ls())
 library(ComplexHeatmap)
+library(circlize) # for colorRamp2
 source("./code/R/01_load_ps.R")
 source("./code/R/02_metab_and_DA.R")
 
@@ -20,13 +21,13 @@ row_fontsize <- 10
 col_fontsize <- 11
 
 # Pseudo count to add when converting to log
-# (choose based on detection limit)
+# enables log(0)
 pseudo <- 1e-6  
 
 # ------ Define Data ------
 
 # Relative Abundance
-rel_wide <- get_rel_wide(ps) %>%
+rel_df <- get_rel_wide(ps) %>%
   # Arrange taxa from largest to smallest abundance
   mutate(row_sum = rowSums(.)) %>%
   arrange(desc(row_sum)) %>%
@@ -34,55 +35,26 @@ rel_wide <- get_rel_wide(ps) %>%
   head(., n = n_show) %>%
   dplyr::select(-row_sum) 
 
-## Check what percent of relative abundance is included in plot
-# sum per sample
-rel_sum <- colSums(rel_wide)
+# sum of relative abundance in each sample
+rel_sum <- colSums(rel_df)
 
 # Convert to log
-data_mat <- as.matrix(rel_wide) %>%
-  { log10(. + pseudo) } 
+log_mat <- as.matrix(rel_df) %>%
+  { log10(. + pseudo) }
 
 # Metabolism
-m_df <- rel_wide %>%
+m_df <- rel_df %>%
   rownames_to_column(var = "Genus") %>%
   get_metabolism(., metab_fname) 
 
-# DA taxa
-DA_taxa <- get_ancom_taxa(ancom_fname, ps, p_threshold)
-
 
 #### ---- Plotting ------
-
-n_cols <- ncol(data_mat)
-n_rows <- nrow(data_mat)
-split = rep(1:n_sizes, each = n_replicates)
-
-# Labels
-row_labels <- rownames(data_mat)
-# italicize species + _g, but not _f/_o/_c/_p
-italic_rows <- !grepl("^(Unk|midas)", row_labels) 
-# bold significant taxa
-bold_rows <- row_labels %in% DA_taxa
-# Apply
-row_fontface <- ifelse(
-  bold_rows & italic_rows, "bold.italic",
-  ifelse(bold_rows, "bold",
-         ifelse(italic_rows, "italic", "plain"))
-)
-
-## Colors
-# Relative Abundance
-ht_colors <- met.brewer(taxa_pal, type = "continuous")
-# Size
-sz_colors <- c(rep("lightgray", n_sizes)) 
-# Metabolism
-m_colors  <- c("P" = "#66C24A", "V" = "#EAEC3F") 
 
 # size annotation
 size_annot <- HeatmapAnnotation(
   sz = anno_block(
     gp = gpar(
-      fill = sz_colors,
+      fill = c(rep("lightgray", n_sizes)), # all the same color
       col = NA # removes border
     ),
     labels = size$name,
@@ -94,8 +66,12 @@ size_annot <- HeatmapAnnotation(
 )
 
 # metabolism annotation
+m_colors  <- c("P" = "#66C24A", "V" = "#EAEC3F") 
 m_annot <- rowAnnotation(
   df = m_df,
+  # column names
+  annotation_name_side = "bottom",
+  annotation_name_rot = -60,
   # color
   col = col_list <- setNames(
     rep(list(m_colors), ncol(m_df)),
@@ -103,31 +79,57 @@ m_annot <- rowAnnotation(
   ),
   na_col = NA, # no color for NA
   # legend
-  show_legend = c(TRUE, c(rep(FALSE, length(m_df)-1))),  # Only first column contributes
-  annotation_legend_param = list(
-    title = NULL, 
-    at = c("P", "V"),
-    labels = c("Positive", "Variable"),
-    nrow = 2
-  ),
-  annotation_name_side = "bottom",
-  annotation_name_rot = -60
+  show_legend = FALSE
+)
+# metabolism legend
+lgd <- Legend(
+  title = NULL,
+  labels = c("Positive", "Variable"),
+  legend_gp = gpar(fill = m_colors),
+  nrow = 2,
+  row_gap = unit(3, "mm")
 )
 
+## Relative Abundance
+
+# Labels
+row_labels <- rownames(log_mat)
+# italicize species + _g, but not _f/_o/_c/_p
+italic_rows <- !grepl("^(Unk|midas)", row_labels) 
+# Apply
+row_fontface <- ifelse(italic_rows, "italic", "plain")
+
+# Dimensions
+n_cols <- ncol(log_mat)
+n_rows <- nrow(log_mat)
+split = rep(1:n_sizes, each = n_replicates)
+
+# Legend Colors
+ht_colors <- met.brewer(taxa_pal, type = "continuous")
+# Define corresponding relative and log values
+# breaks_log <- seq(min(log_mat), max(log_mat), length.out = length(ht_colors))
+# ht_col_fun <- colorRamp2(breaks_log, ht_colors)
+
+# Display legend ticks
+breaks_rel_display <- c(0, 0.1, 1, 10, 25)
+breaks_log_display <- log10(breaks_rel_display + pseudo)
+
 ht <- Heatmap(
-  data_mat,
+  log_mat,
   # columns
-  column_title = NULL, # "Relative Abundance",
+  column_title = NULL, #"Relative Abundance",
   cluster_columns = FALSE, # changes sample order
   show_column_names = FALSE,
   column_split = split, # put a gap between sizes
   # heatmap legend
+  col = ht_colors, #ht_col_fun,
   show_heatmap_legend = TRUE, 
-  col = ht_colors,
   heatmap_legend_param = list(
-    title = "Log (%)", 
+    at = breaks_log_display,
+    labels = breaks_rel_display,
+    title = NULL, #"Log (%)",
     direction = "horizontal",
-    title_position = "lefttop"
+    legend_width = unit(7.5, "cm")
   ),
   # Annotations
   bottom_annotation = size_annot,
@@ -144,9 +146,10 @@ png(fname_rel,
     width = 7,  # width in inches; can adjust
     height = 8, # height in inches; can adjust
     units = "in", res = 300)
-draw(ht, heatmap_legend_side = "top", annotation_legend_side = "top") 
+draw(ht, heatmap_legend_side = "top") #, annotation_legend_side = "top") 
+draw(lgd, x = unit(0.66, "npc"), y = unit(0.98, "npc"), just = c("right", "top"))
 dev.off()
 
-# min and max of all sample sums
+## Check what percent of relative abundance is included in plot
 message(paste("Heatmap Min:", round(min(rel_sum), 2), "%"))
 message(paste("Heatmap Max:", round(max(rel_sum), 2), "%"))
