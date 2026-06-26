5c50562741ac46a314dae9f7449f070b3440c6df
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-06-26 14:50:36 -0500

    remove XS

--- code/R/plot_eps.R
+++ code/R/plot_eps.R
@@ -9,6 +9,7 @@ fname_polys <- paste0("./data/EPS/PS_conc.rds")
 # Calculate average and std of replicates
 group_data <- function(fname) {
   df <- readRDS(fname) %>%
+    filter(size != "XS") %>%
     group_by(size, extract) %>%
     summarize(
       avg = mean(C_VSS),
