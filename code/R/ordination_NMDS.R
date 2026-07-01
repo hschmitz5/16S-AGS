3a1b14d6fbd08889db1b2d9c8e3b9a52b0cc896c
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-07-01 10:44:52 -0500

    added nmds

---
+++ code/R/ordination_NMDS.R
@@ -0,0 +1,48 @@
+rm(list = ls())
+library(phyloseq)
+library(tidyverse)
+library(vegan)
+library(writexl)
+source("./code/R/01_load_ps.R")
+
+# load phyloseq object for all sample sizes
+ps <- readRDS("./data/phyloseq/ps_ASV.rds")
+
+metadata <- data.frame(sample_data(ps)) %>%
+  rownames_to_column(var = "SampleID")
+
+# rows are samples, columns are OTUs
+otu_matrix <- data.frame(t(otu_table(ps)))
+
+# define minimum depth to rarefy
+rarefy_level <- min(sample_sums(ps))  # lowest number of ASVs per sample
+
+dist_matrix <- avgdist(otu_matrix, sample = rarefy_level, iterations = 10, dmethod = "bray")
+
+set.seed(1)
+nmds <- metaMDS(dist_matrix) # list
+
+nmds_df <- scores(nmds) %>%
+  as_tibble(rownames = "SampleID") %>%
+  left_join(., metadata, by = "SampleID")
+
+# ------ Plot ------
+
+# symbol
+# 16 = filled circle, 17 = triangle, 15 = square, 18 = diamond, etc.
+shapes <- c(16, 17, 15, 18, 3, 7)
+
+# colors
+cols <- c("gray", met.brewer(size_pal, n_sizes))
+
+p <- ggplot(nmds_df, aes(NMDS1, NMDS2, color = size.name, shape = size.name)) +
+  geom_point() +
+  geom_polygon(alpha = 0.5, aes(fill = size.name)) +
+  scale_color_manual(values = cols) +
+  scale_shape_manual(values = shapes) +
+  scale_fill_manual(values = cols) +
+  labs(shape = "Size", color = "Size", fill = "Size") +
+  theme_classic(base_size = 12) 
+
+fname <- "./figures/ordination_NMDS.png"
+ggsave(fname, plot = p, width = 6.5, height = 3, dpi = 300)
