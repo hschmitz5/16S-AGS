82a7a942ef797cf3c175dab60bf87f3e7ad215b9
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-07-07 13:44:00 -0500

    add correlation to size

--- code/R/plot_eps.R
+++ code/R/plot_eps.R
@@ -37,12 +37,12 @@ df_wide <- left_join(
 df <- bind_rows(
   'Protein (PN)' = PN,
   'Polysaccharide (PS)' = PS,
-  'PN + PS' = df_wide %>% select(size, extract, avg = total, sd),
+  'Total EPS (PN + PS)' = df_wide %>% select(size, extract, avg = total, sd),
   .id = "assay"
   ) %>%
   mutate(
     extract = recode(extract,"LB" = "Loosely Bound","TB" = "Tightly Bound"),
-    assay = factor(assay, levels = c("Polysaccharide (PS)", "Protein (PN)", "PN + PS"))
+    assay = factor(assay, levels = c("Polysaccharide (PS)", "Protein (PN)", "Total EPS (PN + PS)"))
     ) 
 
 # Calculate PN/PS
@@ -52,24 +52,57 @@ PNPS <- df_wide %>%
     extract = recode(extract,"LB" = "Loosely Bound","TB" = "Tightly Bound")
     ) 
 
+# ------ Correlation ------
+
+# define sample names
+sz <- data.frame(
+  size = c("S", "M", "L", "XL", "XXL"),
+  midpoint = c(1.125, 1.7, 2.4, 3.4, 4.5)
+)
+
+df_wide <- df_wide %>%
+  left_join(., sz, by = "size")
+
+vars <- c("PN_avg", "PS_avg", "PNPS", "total") # rows
+
+correlate_EPS <- function(df_wide, extract_type) {
+  df_extract <- df_wide %>% filter(extract == extract_type)
+  
+  res_extract <- vars |>
+    set_names() |>
+    map_dfr(
+      \(x) broom::tidy(
+        cor.test(df_extract$midpoint, df_extract[[x]], method = "spearman")
+      ),
+      .id = "var"
+    ) |>
+    mutate(
+      extract = extract_type,
+      p.adj = p.adjust(p.value, method = "BH")
+    ) |>
+    select(extract, var, estimate, p.value, p.adj)
+}
+
+res_LB <- correlate_EPS(df_wide, "LB")
+res_TB <- correlate_EPS(df_wide, "TB")
+
+
+# ------ Plot ------
+
 # Determine maximum avg + sd
 max_y1 <- df %>%
-  filter(assay != "PN + PS") %>%
+  filter(assay != "Total EPS (PN + PS)") %>%
   summarise(max_y = max(avg + sd)) %>%
   pull(max_y)
 
 max_y2 <- df %>%
-  filter(assay == "PN + PS") %>%
+  filter(assay == "Total EPS (PN + PS)") %>%
   summarise(max_y = max(avg)) %>%
   pull(max_y)
 
 max_y <- ceiling(
   max(max_y1, max_y2)
   )
-
-# -------------------------------
-
-# Make Plot
 
 p <- ggplot(data = df, aes(x = size, y = avg, fill = assay)) +
   geom_col(position = "dodge", width = 0.8) +
@@ -89,7 +122,7 @@ p <- ggplot(data = df, aes(x = size, y = avg, fill = assay)) +
     values = c(
       "Polysaccharide (PS)" = "lightsalmon2",
       "Protein (PN)"        = "lightblue",
-      "PN + PS"      = "steelblue" 
+      "Total EPS (PN + PS)"      = "steelblue" 
     )
   ) +
   theme_classic(base_size = 12) +
