393cb48e8da5301748f7a7828fd645bd90ccecab
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-07-13 12:11:17 -0500

    removed legend and different colors

--- code/R/plot_mass_percent.R
+++ code/R/plot_mass_percent.R
@@ -9,45 +9,42 @@ x_edges <- c(0, 0.21, 0.43, 0.60, 1.4, 2.0, 2.8, 4.0, 5.0)   # sieve boundaries
 
 mass_percent <- c(27.29, 19.05, 5.05, 11.88, 7.59, 9.85, 10.62, 8.67)
 
-colors <- c("#606060", "white", "gray", met.brewer(size_pal, n_sizes))
+# colors <- c("#606060", "white", "gray", met.brewer(size_pal, n_sizes))
 
 df <- data.frame(
   xmin = x_edges[-length(x_edges)],  # removes last element
   xmax = x_edges[-1],                # removes first element 
   ymin = 0,
   ymax = mass_percent,
-  label = factor(legend_labels, levels = legend_labels),
-  color = colors 
+  label = factor(legend_labels, levels = legend_labels)
+  # color = colors 
 )
 
 # Plot --------------------------------------------------------------
 
 p <- ggplot(df) +
   geom_rect(
-    aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, fill = label), 
-    color = "black", linewidth = 0.2  # outline
+    aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax), 
+    fill = "lightgray", color = "black", linewidth = 0.2  
   ) +
-  scale_fill_manual(values = colors) +
   # control x-axis tick labels
   scale_x_continuous(
-    breaks = x_edges[-c(2, 3, length(x_edges))], # rewove these indices
-    labels = x_edges[-c(2, 3, length(x_edges))]
+    breaks = x_edges[-length(x_edges)], 
+    labels = x_edges[-length(x_edges)]
   ) +
   labs(
-    x = "Biomass Size (mm)",
-    y = "Mass Fraction (%)",
-    fill = NULL
+    x = "Size (mm)",
+    y = "Mass Fraction (%)"
   ) +
   theme_classic(base_size = 14) +
   theme(
-    legend.key.spacing.y = unit(0.1, "cm"),
-    legend.text = element_text(size = 10)
+    axis.text.x = element_text(angle = 45, hjust = 1)
   ) 
 
 # Save figure -------------------------------------------------------
 
 fname <- "./figures/mass-percent.png"
-ggsave(fname, p, width = 6.5, height = 3.5, dpi = 300)
+ggsave(fname, p, width = 6.5, height = 4, dpi = 300)
 
 # Display
 print(p)
